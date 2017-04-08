29fdb345e6deb442edd93f7b6f764af87093ce8e
horacio <hmk142@hotmail.com>
2017-04-08 04:44:50 -0300

    remove user limit

--- src/Sockets.cpp
+++ src/Sockets.cpp
@@ -121,21 +121,21 @@ void DakaraSocketEvents::onSocketNew(dakara::Socket* s) {
 
 		std::string ip(s->getIP());
 
-		if (BanIpBuscar(ip)) {
-			s->close(true);
-			return;
-		}
-
-		if (!IpSecurityAceptarNuevaConexion(ip)) {
-			s->close(true);
-			return;
-		}
-
-		if (IPSecuritySuperaLimiteConexiones(ip)) {
-			IpRestarConexion(ip);
-			s->close(true);
-			return;
-		}
+//		if (BanIpBuscar(ip)) {
+//			s->close(true);
+//			return;
+//		}
+//
+//		if (!IpSecurityAceptarNuevaConexion(ip)) {
+//			s->close(true);
+//			return;
+//		}
+//
+//		if (IPSecuritySuperaLimiteConexiones(ip)) {
+//			IpRestarConexion(ip);
+//			s->close(true);
+//			return;
+//		}
 
 		UserList[UserIndex].incomingData.reset(new clsByteQueue());
 		UserList[UserIndex].outgoingData.reset(new clsByteQueue());
